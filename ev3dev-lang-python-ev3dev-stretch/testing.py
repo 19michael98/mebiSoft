eb5b9d25bf481a188c92d04c2ac4bc826609b60a
Michael Neuhold <michi.neuhold@gmail.com>
2019-03-21 10:40:31 +0100

    motor tests

--- ev3dev-lang-python-ev3dev-stretch/testing.py
+++ ev3dev-lang-python-ev3dev-stretch/testing.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-from ev3dev2.motor import LargeMotor, OUTPUT_C, OUTPUT_A, OUTPUT_B, SpeedPercent, MoveTank
+from ev3dev2.motor import LargeMotor, MediumMotor, OUTPUT_C, OUTPUT_A, OUTPUT_B, OUTPUT_D, SpeedPercent, MoveTank
 from ev3dev2.sensor import INPUT_1
 from ev3dev2.sensor.lego import TouchSensor
 from ev3dev2.sensor.lego import InfraredSensor
@@ -17,22 +17,36 @@ import os
 
 # TODO: Add code here
 
-cS = ColorSensor(INPUT_1)
-while true:
-  while cS.color() == 5:
-    tank_drive = MoveTank(OUTPUT_A,OUTPUT_B);
-    tank_drive.on_for_rotations(SpeedPercent(50), SpeedPercent(50), 10)
+#cS = ColorSensor(INPUT_1)
+#while true:
+ # while cS.color() == 5:
 
+#Fahren
+tank_drive = MoveTank(OUTPUT_A,OUTPUT_B);
+tank_drive.on_for_rotations(SpeedPercent(50), SpeedPercent(50), 10)
 
+#Greifer aufmachen
+m2 = MediumMotor(OUTPUT_D)
+m2.on_for_rotations(SpeedPercent(10),1)
+#Greifer zumachen
+m2 = MediumMotor(OUTPUT_D)
+m2.on_for_rotations(SpeedPercent(-10),1)
 
+#Greifer Heben
+m1 = LargeMotor(OUTPUT_C)
+m1.on_for_rotations(SpeedPercent(20), 1)
+
+#Greifer aufmachen
+m2 = MediumMotor(OUTPUT_D)
+m2.on_for_rotations(SpeedPercent(10),1)
+
+#Drehen rechts/links
 #m1 = LargeMotor(OUTPUT_A)
 #m1.on_for_rotations(SpeedPercent(75), 20)
 
 #m2 = LargeMotor(OUTPUT_B)
 #m2.on_for_rotations(SpeedPercent(75), 20)
 
-#m3 = SmallMotor(OUTPUT_C)
-#m3.on_for_rotations(SpeedPercent(20), 0.5)
 
 
 
